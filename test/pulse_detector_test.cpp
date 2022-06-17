b375610d816ede33d4f5c1edec162e731cfc4462
dawkrz13 <dawkrz13@gmail.com>
2022-06-17 08:15:20 +0200

    pulse detector test added

--- test/pulse_detector_test.cpp
+++ test/pulse_detector_test.cpp
@@ -1,7 +1,10 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stdint.h>
+#include <vector>
 
 extern "C"
 {
@@ -9,13 +12,28 @@ extern "C"
 }
 
 using namespace std;
+using ::testing::AllOf;
+using ::testing::Gt;
+using ::testing::Le;
 
-int main(void)
+static uint32_t count_average(const vector<uint32_t> pd_output)
+{
+    uint32_t sum_of_elems;
+
+    for(auto& n : pd_output)
+        sum_of_elems += n;
+
+    return sum_of_elems/pd_output.size();
+}
+
+TEST(PulseDetector, common)
 {
     PULSE_DETECTOR pd = {0};
+    vector<uint32_t> pd_output;
+    uint32_t ref_bpm = 60; // reference average bpm as calculated by model
 
     fstream file;
-    file.open("ir_raw.txt", ios::in);
+    file.open("data/ir_raw.txt", ios::in);
     if(file.is_open())
     {
         string ir_buffer;
@@ -26,11 +44,13 @@ int main(void)
             measure_heart_rate(&pd, ir);
             if(pd.state == STATE_READY)
             {
-                cout << pd.bpm << endl;
+                pd_output.push_back(pd.bpm);
             }
         }
         file.close();
     }
 
-    return 0;
+    uint32_t average_bpm = count_average(pd_output);
+
+    ASSERT_THAT(average_bpm, AllOf(Gt(ref_bpm-2), Le(ref_bpm+2))) << "Upss...";
 }
